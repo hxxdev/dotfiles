f13294a22ebb6b220aa13ed449280a73652aa168
hxxdev <hxx.dev@gmail.com>
2025-07-18 00:13:44 +0900

    updated mini config.

--- nvim/.config/nvim/lua/config/config_mini.lua
+++ nvim/.config/nvim/lua/config/config_mini.lua
@@ -1,21 +1,46 @@
-require('mini.ai').setup {
-    n_lines = 500, -- max 500 lines
-}
+--require('mini.ai').setup()
 
--- mini.surround
 require('mini.files').setup()
+
+-- visual mode sa ": wrap with "
+-- normal mode sd ': remove surrounding ''
+-- normal mode sr '(: replace surrounding '' with ()
 require('mini.surround').setup()
 
--- mini.align
 require('mini.align').setup()
 
--- mini.statusline
-local statusline = require 'mini.statusline'
+require('mini.indentscope').setup()
 
-statusline.setup {
-    use_icons = vim.g.have_nerd_font,
+-- normal mode gcc: comment line
+-- visual mode gc: comment
+require('mini.comment').setup()
+
+-- :lua MiniTrailspace.trim(): trim all trailing whitespaces.
+-- :lua MiniTrailspace.trim_last_lines(): trim all trailing empty lines.
+require('mini.trailspace').setup()
+
+require('mini.jump').setup {
+    delay = {
+        -- Delay(ms) between jump and highlighting all possible jumps
+        highlight = 10000000,
+
+        -- Delay(ms) between jump and automatic stop if idle (no jump is done)
+        idle_stop = 10000000,
+    },
 }
 
-statusline.section_location = function()
-    return '%2l:%-2v'
-end
+-- normal mode gS: split join toggle
+require('mini.splitjoin').setup()
+
+require('mini.pairs').setup()
+
+require('mini.starter').setup()
+
+-- require('mini.pick').setup()
+
+-- mini.statusline
+-- local statusline = require 'mini.statusline'
+-- statusline.setup { use_icons = vim.g.have_nerd_font }
+-- statusline.section_location = function()
+--     return '%2l:%-2v'
+-- end
