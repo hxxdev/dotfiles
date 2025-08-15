a6e7380bcf072fd5af227e181622d86fcd3619d4
hxxdev <hxx.dev@gmail.com>
2025-08-16 03:16:59 +0900

    update overseer.

---
+++ nvim/.config/nvim/lua/overseer/template/user/run_script.lua
@@ -0,0 +1,27 @@
+return {
+    name = "run script",
+    builder = function()
+        local file = vim.fn.expand("%:p")
+        local cmd
+        if vim.bo.filetype == "go" then
+            cmd = { "go", "run", file }
+        elseif vim.bo.filetype == "python" then
+            cmd = { "python", file }
+        elseif vim.bo.filetype == "sh" then
+            cmd = { "bash", file }
+        else
+            cmd = { file }
+        end
+        return {
+            cmd = cmd,
+            components = {
+                { "on_output_quickfix", set_diagnostics = true },
+                "on_result_diagnostics",
+                "default",
+            },
+        }
+    end,
+    condition = {
+        filetype = { "sh", "python", "go" },
+    },
+}
--- nvim/.config/nvim/lua/plugins/overseer.lua
+++ nvim/.config/nvim/lua/plugins/overseer.lua
@@ -3,7 +3,7 @@ return {
     opts = {},
     config = function()
         require("overseer").setup({
-            templates = { "builtin", "user.cpp_build" },
+            templates = { "builtin", "user.cpp_build", "user.run_script" },
         })
     end
 }
