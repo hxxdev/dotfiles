31f84b7b4140e836dca5769fe52973b89cb6d939
hxxdev <hxx.dev@gmail.com>
2025-08-30 21:20:39 +0900

    use zen-browser for markdown-preview.nvim.

--- nvim/.config/nvim/lua/plugins/markdown-preview.lua
+++ nvim/.config/nvim/lua/plugins/markdown-preview.lua
@@ -4,6 +4,7 @@ return {
     ft = { "markdown" },
     config = function()
         vim.keymap.set('n', '<C-m>', ':MarkdownPreview<CR>', { noremap = true, silent = true })
+        vim.g.mkdp_browser = 'zen-browser'
     end,
     build = function() vim.fn["mkdp#util#install"]() end,
 }
