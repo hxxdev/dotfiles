c01ed0277e2b315e1b9c7120ea4f09195745770a
hxxdev <hxx.dev@gmail.com>
2026-05-04 01:34:26 +0900

    themes refactoring

--- nvim/.config/nvim/lua/themes.lua
+++ nvim/.config/nvim/lua/themes.lua
@@ -1,3 +1,4 @@
 -- vim.cmd.colorscheme "catppuccin-nvim"
-vim.cmd.colorscheme "kanso-zen"
+-- vim.cmd.colorscheme "kanso-zen"
+vim.cmd.colorscheme "github_dark_default"
 -- vim.cmd.colorscheme "rose-pine"
