f5238a009bd289400adf0b8af0aba1029f3a8132
hxxdev <hxx.dev@gmail.com>
2026-06-13 02:36:47 +0900

    clear lsp log on launch

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,4 +1,9 @@
--- Setup plugins via vim.pack.add() (requires Neovim 0.12+)
+-- Clear the LSP log file on every Neovim launch
+local lsp_log_path = vim.lsp.log.get_filename()
+local f = io.open(lsp_log_path, "w")
+if f then
+  f:close()
+end
 require("plugins")
 
 require("themes")
